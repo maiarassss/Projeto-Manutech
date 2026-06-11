671516531b59647be3da108a1f126309df3d4591
otavio_novasco <otavio_novasco@estudante.sesisenai.org.br>
2026-06-10 22:06:10 -0300

    criação das páginas

--- manutech/src/App.vue
+++ manutech/src/App.vue
@@ -1,9 +1,141 @@
-<template>
-  <nav>
-    <RouterLink to="/">Início</RouterLink>
-    <RouterLink to="/ordens">Ordens</RouterLink>
-    <RouterLink to="/tecnicos">Técnicos</RouterLink>
-  </nav>
+<script setup>
+import { ref } from "vue";
 
-  <router-view />
+const sidebarOpen = ref(false);
+</script>
+
+<template>
+  <header class="navbar">
+    <button class="menu-button" @click="sidebarOpen = !sidebarOpen">☰</button>
+
+    <h1>Bem-vindo à MANUTECH</h1>
+
+    <input type="text" placeholder="Pesquisar..." class="search-bar" />
+  </header>
+
+  <div class="container">
+    <!-- Sidebar -->
+    <aside class="sidebar" :class="{ active: sidebarOpen }">
+      <RouterLink to="/"> Início </RouterLink>
+
+      <RouterLink to="/tecnicos"> Técnicos </RouterLink>
+
+      <RouterLink to="/ordens"> Ordens </RouterLink>
+
+      <RouterLink to="/maquinas"> Máquinas </RouterLink>
+    </aside>
+
+    <main class="content" :class="{ shifted: sidebarOpen }">
+      <RouterView />
+    </main>
+  </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Segoe UI;
+  background: #f4f6f9;
+}
+
+.navbar {
+  height: 70px;
+  background: #1565c0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 30px;
+
+  color: white;
+}
+
+.navbar h1 {
+  flex: 1;
+  margin-left: 20px;
+  color: white;
+}
+
+.menu-button {
+  background: none;
+  border: none;
+  color: white;
+  font-size: 28px;
+  cursor: pointer;
+}
+
+.search-bar {
+  width: 250px;
+  height: 40px;
+
+  border: none;
+  border-radius: 10px;
+
+  padding-left: 15px;
+
+  outline: none;
+}
+
+/* CORPO */
+
+.container {
+  display: flex;
+}
+
+.sidebar {
+  position: fixed;
+
+  top: 70px;
+  left: -250px;
+
+  width: 250px;
+  height: 100vh;
+
+  background: #1e293b;
+
+  display: flex;
+  flex-direction: column;
+
+  padding-top: 30px;
+
+  transition: 0.3s;
+}
+
+.sidebar.active {
+  left: 0;
+}
+
+.sidebar a {
+  color: white;
+
+  text-decoration: none;
+
+  padding: 20px;
+
+  font-size: 18px;
+
+  transition: 0.3s;
+}
+
+.sidebar a:hover {
+  background: #334155;
+}
+
+.content {
+  width: 100%;
+
+  padding: 40px;
+
+  transition: 0.3s;
+}
+
+.content.shifted {
+  margin-left: 250px;
+}
+</style>
---
+++ manutech/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="navbar">
+    <h1>MANUTECH</h1>
+  </header>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: #111184;
+  color: white;
+  height: 80px;
+
+  display: flex;
+  align-items: center;
+
+  padding-left: 30px;
+}
+
+h1 {
+  font-size: 2rem;
+}
+</style>
