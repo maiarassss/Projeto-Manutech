a4b3df7f7e28fe4b16e9693873cf831109d87dda
Brayan_marcos <brayan_marcos@estudante.sesisenai.org.br>
2026-06-17 22:14:08 -0300

    atualização no codigo

--- backend/ManuTech/src/main/java/com/manutech/ManuTech/service/OrdemServicoService.java
+++ backend/ManuTech/src/main/java/com/manutech/ManuTech/service/OrdemServicoService.java
@@ -1,18 +1,25 @@
 package com.manutech.ManuTech.service;
 
+import com.manutech.ManuTech.dto.OrdemServicoRequestDTO;
 import com.manutech.ManuTech.dto.OrdemServicoResponseDTO;
+import com.manutech.ManuTech.exception.RecursoNaoEncontradoException;
 import com.manutech.ManuTech.model.OrdemServico;
+import com.manutech.ManuTech.repository.MaquinaRepository;
+import com.manutech.ManuTech.repository.OrdemServicoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrdemServicoService {
-    private final OrdemServicoService repository;
-    //private final MaquinaService maquinaRepository;
+
+    private final OrdemServicoRepository repository;
+    private final MaquinaRepository maquinaRepository;
 
 
-    // public OrdemServicoService(OrdemServicoRepository repository){// MaquinaRepository maquinaRepository
-    //      this.repository = repository;
-    // this.maquinaRepository = maquinaRepository;
+     public OrdemServicoService(OrdemServicoRepository repository, MaquinaRepository maquinaRepository){
+         this.repository = repository;
+     this.maquinaRepository = maquinaRepository;
 
 }
 
@@ -21,47 +28,66 @@ private OrdemServicoResponseDTO toResponseDTO(OrdemServico ordemServico){
             ordemServico.getIdOrdem(),
             ordemServico.getTitulo(),
             ordemServico.getDescricao(),
-            ordemServico.getPrioridade()//,
-            //ordemServico.getMaquina()
+            ordemServico.getPrioridade(),
+            ordemServico.getStatus(),
+
+            ordemServico.getMaquina().getIdMaquina(),
+            ordemServico.getMaquina().getCodigoIdentificador(),
+            ordemServico.getMaquina().getModelo(),
+            ordemServico.getMaquina().getAtiva(),
+            ordemServico.getMaquina().getSetor().getIdSetor(),
+            ordemServico.getMaquina().getSetor().getNomeSetor()
     );
 }
-// public OrdemServicoResponseDTO salvar(OrdemServicoRequestDTO dto){
-// OrdemServico ordemServico = new OrdemServicoRepository.findById(dto.idOrdemServico())
-//       .orElseThrow(() -> new RecursoNaoEncontradoException("Ordem do serviço não encontrada"));
+ public OrdemServicoResponseDTO salvar(OrdemServicoRequestDTO dto){
+
 
 OrdemServico ordemServico = new OrdemServico();
-//  ordemServico.setTitulo(dto.titulo);
-//  ordemServico.setDescricao(dto.descricao);
-//   ordemServico.setPrioridade(dto.prioridade);
-//   ordemServico.setMaquina(Maquina);
 
-//   OrdemServico salva = repository.save(ordemServico);
-//    return toResponseDTO(salva);
-//}
+  ordemServico.setTitulo(dto.titulo());
+  ordemServico.setDescricao(dto.descricao());
+  ordemServico.setPrioridade(dto.prioridade());
+  ordemServico.setStatus(dto.status());
 
-// public List<OrdemServicoResponseDTO> listarPorMaquina(Long idMaquina){
-//  maquinaRepository.findById(idMaquina)
-//        .orElseThrow(() -> new RecursoNaoEncontradoException("maquina não encontrada"));
 
-//  return repository.findByMaquinaIdMaquina(idMaquina)
-//       .stream()
-//      .map(this::toResponseDTO)
-//       .toList();
-// }
 
-//  public OrdemServicoResponseDTO buscarId(Long idOrdemServico){
-//    OrdemServico ordemServico = repository.findById(idOrdemServico).orElseThrow(()
-//            -> new RecursoNaoEncontradoException("Ordem serviço não foi encontrada"));
-//    return toResponseDTO(ordemServico);
-//}
+   OrdemServico salvar = repository.save(ordemServico);
+    return toResponseDTO(salvar);
+}
 
-// public void deletar(Long idOrdemServico){
-//    OrdemServico ordemServico = repository.findById(idOrdemServico).orElseThrow(() ->
-//           new RecursoNaoEncontradoException("Ordem serviço não encontrado"));
-//   repository.delete(ordemServico);
-//}
+ public List<OrdemServicoResponseDTO> listarTodos(){
+  return repository.findAll()
+      .stream()
+      .map(this::toResponseDTO)
+       .toList();
+ }
 
-//}
+  public OrdemServicoResponseDTO buscarId(Long idOrdemServico){
+   OrdemServico ordemServico = repository.findById(idOrdemServico).orElseThrow(()
+           -> new RecursoNaoEncontradoException("Ordem serviço não foi encontrada"));
+   return toResponseDTO(ordemServico);
+}
+
+ public void deletar(Long idOrdemServico) {
+   OrdemServico ordemServico = repository.findById(idOrdemServico)
+           .orElseThrow(() ->
+           new RecursoNaoEncontradoException("Ordem serviço não encontrado"));
+
+   repository.delete(ordemServico);
+}
+
+    public OrdemServicoResponseDTO atualizar(Long idOrdemService, OrdemServicoRequestDTO dto) {
+        OrdemServico existente = repository.findById(idOrdemService).orElseThrow(() ->
+              new RecursoNaoEncontradoException("Ordem de serviço não Encontrada"));
+        existente.setTitulo(dto.titulo());
+        existente.setDescricao(dto.descricao());
+        existente.setPrioridade(dto.prioridade());
+        existente.setStatus(dto.status());
+
+        OrdemServico atulizado = repository.save(existente);
+        return toResponseDTO(atulizado);
+    }
+}
 
 
 
