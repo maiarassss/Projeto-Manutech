1efbac8c1fb3d73f496d60534bd668ebf352fcda
Julia <juliafcostads@gmail.com>
2026-06-29 20:39:50 -0300

    Tecnico: ajustes nas validações

--- backend/ManuTech/src/main/java/com/manutech/ManuTech/dto/TecnicoRequestDTO.java
+++ backend/ManuTech/src/main/java/com/manutech/ManuTech/dto/TecnicoRequestDTO.java
@@ -2,6 +2,7 @@ package com.manutech.ManuTech.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ public record TecnicoRequestDTO(
         @NotBlank(message = "*Informe o nome do técnico")
         String nome,
 
+        @NotBlank(message = "*Informe o telefone do técnico")
         String telefone,
 
         //uma lista para registrar mais de um setor por técnico, que não pode estar vazia
--- backend/ManuTech/src/main/java/com/manutech/ManuTech/repository/TecnicoRepository.java
+++ backend/ManuTech/src/main/java/com/manutech/ManuTech/repository/TecnicoRepository.java
@@ -9,6 +9,8 @@ public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
 
     List<Tecnico> findByNomeContainingIgnoreCase(String nome);
 
+    boolean existsByNomeIgnoreCase(String nome);
+
     //busca os técnicos de determinado setor
     List<Tecnico> findBySetoresAtendidosIdSetor(Long idSetor);
 }
